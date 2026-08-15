ef541e971323c4e23ad09e1e7b6db1f6727debe3
KC Abhinav Namboothiripad <abhinav.n@students.iiit.ac.in>
2026-08-15 18:06:51 +0530

    Refactor calculator functionality and update main logic for user interaction

--- main.cpp
+++ main.cpp
@@ -8,7 +8,20 @@ void printWelcomeMessage() {
 
 void greetUser() { cout << "Hello, Developer!" << std::endl; }
 
+void printGoodbyeMessage() { cout << "Goodbye, Developer!" << std::endl; }
+
 int addNumbers(int a, int b) { return a + b; }
+
+int calculator(int a, int b, int op) { 
+    switch (op) {
+        case 1: return a + b;
+        case 2: return a - b;
+        case 3: return a * b;
+        case 4: return a / b;
+        default: return 0;
+    }
+
+}
 
 int main() {
   printWelcomeMessage();
@@ -16,11 +29,15 @@ int main() {
 
   int num1 = 5;
   int num2 = 10;
-  int result = addNumbers(num1, num2);
-
-  cout << "The sum of " << num1 << " and " << num2 << " is: " << result
-       << endl;
-  cout << "Program finished successfully." << endl;
+  cout<<"Calculator Operations:"<<endl;
+  cout<<"1. Addition"<<endl;
+  cout<<"2. Subtraction"<<endl;
+  cout<<"3. Multiplication"<<endl;
+  cout<<"4. Division"<<endl;
+  int op;
+  cin>>op;
+  int result = calculator(num1, num2, op);
+  printGoodbyeMessage();
 
   return 0;
 }
