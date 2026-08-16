0c3a8a85732292c4f45abff8c952090d8edb1b6e
KC Abhinav Namboothiripad <abhinav.n@students.iiit.ac.in>
2026-08-16 10:28:40 +0530

    New feature: Add area functions to calc

--- areas.cpp
+++ areas.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 
+
+using namespace std;
+
+int area(int length, int width) {
+    return length * width;
+}
+int area(int side) {
+    return side * side;
+}
+
+int area(int base, int height) {
+    return 0.5 * base * height;
+}
+
 int areaOfCircle(int radius) {
     return 3.14 * radius * radius;
 }
