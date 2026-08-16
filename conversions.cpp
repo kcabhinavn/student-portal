9da88fb29cda4ad56882a9b14c2fe05bec11add7
KC Abhinav Namboothiripad <abhinav.n@students.iiit.ac.in>
2026-08-16 15:58:14 +0530

    Add temperature conversion functions: Celsius to Fahrenheit, Fahrenheit to Celsius, and Celsius to Kelvin

---
+++ conversions.cpp
@@ -0,0 +1,13 @@
+
+
+double celsiusToFahrenheit(double celsius) {
+    return (celsius * 9.0 / 5.0) + 32;
+}
+
+double fahrenheitToCelsius(double fahrenheit) {
+    return (fahrenheit - 32) * 5.0 / 9.0;
+}
+
+double celsiusToKelvin(double celsius) {
+    return celsius + 273.15;
+}
